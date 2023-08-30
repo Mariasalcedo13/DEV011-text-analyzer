d120f7f49337ce32ee66c42398129d99d06c73f5
maria salcedo <mayi13sa02@hotmail.com>
2023-08-30 13:56:14 -0500

    se le dieron funcionalidad a  todas las metricas

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,80 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    if (text.trim() === "") {
+      return 0;
+    } else {
+      const contarPalabras = text.trim().split(/\s+/);
+      return contarPalabras.length;
+    }
   },
+ 
+// trim elimina los espacio en blanco en una cadena de texto
+// split se usa para dividir una cadena en parte mas pequeña (/s+/)  en espacio blanco
+
+
+
+
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
+  // con length retorna la longitud 
+
+
+
+
+
+
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+    const almacenText = text.replace(/[^\w]/g,'');
+    return almacenText.length;
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+
+  // lo que se hizo fue que con la variable almacenText almacenar la cadena de texto despues de eliminar y los signo de puntuacion 
+// busca replace todas las expresiones que no sean letras ni numeros
+
+
+
+getAverageWordLength: (text) => {
+   //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+   if(typeof text !== "string" ){
+    return 0;
+  }
+  const palabraLongitud = text.trim().split(/\s+/);
+  return palabraLongitud.reduce((total, palabra2) => total + palabra2.length,0) / palabraLongitud.length;
+ 
+},
+// trim elimina los espacio en blanco en una cadena de texto
+// split(/s+/) se usa para dividir una cadena en espacio blanc
+// reduce () suma la longitud de las palabras y despues suma y divide para obtener la longitud
+//https://www.google.com/search?q=funci%C3%B3n+de+conteo+de+palabras+en+javascript&source=lmns&bih=602&biw=1332&hl=es-419&sa=X&ved=2ahUKEwjYnPu4j4KBAxUDSDABHbseDo0Q0pQJKAB6BAgBEAI#fpstate=ive&vld=cid:e4dff459,vid:PlqTmim0n7w
+//https://www.jstips.co/es_es/javascript/array-average-and-median/
+  
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const contarNumeros = text.match(/\d+/g) || [];
+    return contarNumeros.length;
   },
+  
+
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numbers =text.match(/\d+/g);
+    if (numbers){
+      const suma = numbers.reduce((acomuladorReduccion , numeroActual)=> acomuladorReduccion + parseInt(numeroActual),0)
+      return suma;
+     
+    }else{
+      return 0;
+    }
   },
 };
-
+//match acomula los numero en un dentro de un texto si se encuentro un numero pasaria reduce()donde ahi se suma y toma el valor inicial en 0
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const userInput = document.querySelector('textarea[name="user-input"]');
+const resetButton = document.getElementById('reset-button');
+const metricasDeCadaItems = document.querySelectorAll('.contenedor-li li');
+
+const pruebaMetricas = () => {
+  const text = userInput.value;
+  metricasDeCadaItems[0].textContent = `Caracteres: ${text.length}`;
+  metricasDeCadaItems[1].textContent = `Caracteres sin Espacio: ${text.replace(/\s/g, '').length}`;
+  metricasDeCadaItems[2].textContent = `Palabras: ${analyzer.getWordCount(text)}`;
+  metricasDeCadaItems[3].textContent = `Números: ${analyzer.getNumberCount(text)}`;
+  metricasDeCadaItems[4].textContent = `Suma Numeros: ${analyzer.getNumberSum(text)}`;
+  metricasDeCadaItems[5].textContent = `Promedio Longitud: ${analyzer.getAverageWordLength(text).toFixed(1)}`;
+};
+
+const resultadoMetricas = () => {
+  userInput.value = '';
+  pruebaMetricas();
+};
+
+userInput.addEventListener('keyup', pruebaMetricas);
+resetButton.addEventListener('click', resultadoMetricas);
+
+pruebaMetricas();
+// se habilita el boton 
+/*let funciónBoton = document.querySelector("textarea");
+funciónBoton.addEventListener("keyup",()=>{
+
+})
+
+let botonMetricas =document.getElementById("reset-button");
+botonMetricas.addEventListener("click",()=>{
+    funciónBoton.value = ""
+})*/
+
